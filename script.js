4ea83f9c864445ef4a6362493cf9cb0c5f25d843
Chuksartinternational <113777505+nwokoriechuks@users.noreply.github.com>
2025-08-21 02:06:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -35,6 +35,39 @@ accHeaders.forEach(header => {
   });
 });
 
+
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.lightbox .close');
+const nextBtn = document.querySelector('.lightbox .next');
+const prevBtn = document.querySelector('.lightbox .prev');
+const galleryImages = document.querySelectorAll('.samples img');
+let currentIndex = 0;
+
+galleryImages.forEach((img, index) => {
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    lightboxImg.src = img.src;
+    currentIndex = index;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % galleryImages.length;
+  lightboxImg.src = galleryImages[currentIndex].src;
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
+  lightboxImg.src = galleryImages[currentIndex].src;
+});
+
+
+
 // Testimonial slider (About page)
 let testimonials = document.querySelectorAll(".testimonial");
 let index = 0;
